3883f1c464c53748b2111858866bf22ef4fd6acc
Vlad Danilchuk <vladdanilchuk9@gmail.com>
2024-05-14 19:19:58 +0300

    add url to detail page

--- Store/urls.py
+++ Store/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from core.views import contact, index
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("", index, name="index"),
     path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
+    path("items/", include("item.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
