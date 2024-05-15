df9ea7a6d5161bffe59b31281f3d8808802b0e04
Vlad Danilchuk <vladdanilchuk9@gmail.com>
2024-05-16 01:30:52 +0300

    add create form

---
+++ item/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+from .models import Item
+
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ["name", "description", "price", "image", "category"]
+        labels = {
+            "name": "Назва",
+            "description": "Опис",
+            "price": "Ціна",
+            "image": "Зображення",
+            "category": "Категорія",
+        }
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control"}),
+            "price": forms.NumberInput(attrs={"class": "form-control"}),
+            "image": forms.FileInput(attrs={"class": "form-control"}),
+            "category": forms.Select(attrs={"class": "form-control"}),
+        }
