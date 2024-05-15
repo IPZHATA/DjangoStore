d22de6db374b60ee59d2fb3d27e4779bf7503c22
Oleh <olehbashtovyi@gmail.com>
2024-05-15 19:39:18 +0300

    checkout.html rework, add checkout function

--- order/views.py
+++ order/views.py
@@ -2,18 +2,41 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from item.models import Item
-from .models import Order, DeliveryProvider, OrderItem
-from .forms import OrderForm, PaymentForm
+from .forms import AddressForm, PaymentForm
 
 
+# def checkout(request):
+#     item = Item.objects.filter(is_sold=False)[0]
+#     items = [item, item, item]
+#
+#     payment_form = PaymentForm()
+#     address_form = AddressForm()
+#
+#     return render(request, "checkout.html",
+#                   {
+#                       "items": items,
+#                       "payment_form": payment_form,
+#                       "address_form": address_form
+#                   })
+
+@login_required()
 def checkout(request):
+
     item = Item.objects.filter(is_sold=False)[0]
     items = [item, item, item]
 
-    payment_form = PaymentForm()
+    if request.method == 'POST':
+        address_form = AddressForm(request.POST, prefix='form_1')
+        payment_form = PaymentForm(request.POST, prefix='form_2')
+        if address_form.is_valid() and payment_form.is_valid():
+            return redirect('item:index')
+    else:
+        address_form = AddressForm(prefix='form_1')
+        payment_form = PaymentForm(prefix='form_2')
 
     return render(request, "checkout.html",
                   {
                       "items": items,
-                      "payment_form": payment_form
+                      "payment_form": payment_form,
+                      "address_form": address_form
                   })
