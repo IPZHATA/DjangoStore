37a7bb64b3ee2b3ad9b830f30bd3ba55abafad33
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2024-05-16 23:43:25 +0300

    add urls

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = "item"
+app_name = 'cart'
 urlpatterns = [
-    path("<int:pk>/", views.detail, name="detail"),
+    path('cart', views.cart, name='cart'),
+    path('cart-add/<int:product_id>', views.cart_add, name='cart-add'),
+    path('cart-remove/<int:product_id>', views.cart_remove, name='cart-remove'),
+    path('cart-remove-all/<int:product_id>', views.cart_remove_completely, name='cart-remove-all')
 ]
