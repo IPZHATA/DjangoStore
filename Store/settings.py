80fe7671d75dc546698a33f75dd521bf057f931c
Oleh <olehbashtovyi@gmail.com>
2024-05-23 12:09:57 +0300

    add login/signup forms

--- Store/settings.py
+++ Store/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['django-store-38382fc9e880.herokuapp.com', 'localhost', '127.0.0.1']
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Application definition
 
