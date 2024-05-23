93d045c01ecb9dde0319a91a1f2a1c5a25fd1a5c
Oleh <olehbashtovyi@gmail.com>
2024-05-23 13:06:07 +0300

    add login/signup pages

--- Store/urls.py
+++ Store/urls.py
@@ -2,12 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
-from core.views import contact, index
+from core.forms import LoginForm
+from core.views import contact, index, signup
 
 urlpatterns = [
     path("", index, name="index"),
     path("contact/", contact, name="contact"),
+    path('signup/', signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
     path("admin/", admin.site.urls),
     path("items/", include("item.urls")),
     path("dashboard/", include("dashboard.urls")),
